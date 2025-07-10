b80c967803780e86cdb7be8b473deda251489bc3
lansylord <lansylord@gmail.com>
2025-07-10 14:12:13 -0300

    Primeiro commit

---
+++ src/main/java/com/rpe/desafio/rpe_api/RpeApiApplication.java
@@ -0,0 +1,13 @@
+package com.rpe.desafio.rpe_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpe/desafio/rpe_api/RpeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpe.desafio.rpe_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
